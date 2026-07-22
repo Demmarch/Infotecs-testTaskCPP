cce9d6295048f134f0efe226caaf02960e77e834
Demmarc <yar310306d@gmail.com>
2026-07-22 04:35:14 +0300

    Basic hpp logger

---
+++ lib/logger.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <mutex>
+
+// Уровень через перечисление с понятными именами
+// enum class чтобы не превращались в числа
+enum class LogLevel {
+    DEBUG = 0,
+    INFO,
+    WARNING,
+    ERROR
+};
+
+class Logger {
+public:
+    // Инициализация логгера
+    Logger(const std::string& filename, LogLevel default_level);
+
+    // Деструктор закроет файл автоматически
+    ~Logger();
+
+    // Запрещаем копирование и присваивание объекта логгера
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    // Требование: возможность менять уровень важности после инициализации
+    void set_level(LogLevel new_level);
+
+    // Главный метод записи в журнал
+    void log(LogLevel level, const std::string& message);
+
+private:
+    std::ofstream file_stream_;
+    LogLevel current_level_;
+};
